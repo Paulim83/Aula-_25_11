84bcb5a066f22b2c1f7a53a4e490cba731bcbcb7
Paulim83 <paulooliveirade@gmail.com>
2025-11-26 00:42:13 +0000

    Teste

---
+++ aula2.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class aula2 {
+
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    // Título 
+    System.out.println("=== Cadastro Simples ===");
+    System.out.println();
+    //Insersões
+    System.out.print("Digite seu nome ");
+    String nome = sc.nextLine();
+    System.out.print( "Digite sua idade");
+    int idade = sc.nextInt();
+    System.out.print("s: Digite sua cidade ");
+    sc.nextLine(); // limpar buffer
+    String cidade = sc.nextLine();
+
+
+    // Linha em branco
+    System.out.println();
+
+    //Impressões
+    System.out.println("===Dados Informados===");
+    System.out.print("Nome: ");
+    System.out.println(nome);
+
+    System.out.print( "Idade: ");
+    System.out.println(idade);
+
+    System.out.print( "Cidade: ");
+    System.out.println(cidade);
+    System.out.println("Salário: ");
+
