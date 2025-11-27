d57128bae356ef45e5395680682b1a9b49d88759
Paulim83 <paulooliveirade@gmail.com>
2025-11-27 00:05:18 +0000

    inserção do meu cálculo

---
+++ calculadeira1.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class calculadeira1{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(" === Calculadeira ===");
+        System.out.println();
+
+        //Entrada do primeiro número
+        System.out.print("Insira o primeiro número da conta: "); 
+        int num1 = sc.nextInt();
+
+        // Entrada do segundo número
+        System.out.print("Insira o segundo número da conta: ");
+        int num2 = sc.nextInt();
+
+        //Soma
+        System.out.println("Soma: " + (num1 + num2) );
+
+        int soma = num1 + num2;
+        System.out.printf("Soma: %d\n" ,soma);
+
+        int sub =num1-num2;
+        System.out.printf("Subtração: %d\n", sub);
+
+        //Multiplicação
+        int mult = num1 * num2;
+        System.out.printf( "Multiplicação: %d\n", mult);
+        
+
+    
+
+    
+
+
+    }
+}
+
+
