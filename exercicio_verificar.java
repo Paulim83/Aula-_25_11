cf3efafd803181a7fc30d43c98b7e91624b5d978
Paulim83 <paulooliveirade@gmail.com>
2025-11-27 00:48:42 +0000

    aula5

---
+++ exercicio_verificar.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class exercicio_verificar {
+    public static void main(String[] args) {
+    Scanner sc =new Scanner(System.in);
+
+    System.out.println( "Verificador de maior idade!");
+    System.out.println();
+
+    System.out.print("Insira sua idade: ");
+    int idade = sc.nextInt();
+
+    if (idade >= 18) {
+     System.out.println("Você é maior de idade!");
+    } else {
+        System.out.println( "Você é menor de idade");
+    }
+    sc.close();
+}
+}
