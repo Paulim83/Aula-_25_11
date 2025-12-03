440be30aaee71813db4dd4d65c46854f13596347
Paulim83 <paulooliveirade@gmail.com>
2025-12-03 23:52:18 +0000

    contagem1000

---
+++ Cont1000.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Cont1000 {
+public static void main(String)[] args) {
+    Scanner sc = new Scanner (System.in);
+    String frase = sc.nextLine();
+    int cont = 1;
+
+    While (cont<= 1000) {
+
+        System.out.println(frase);
+        System.out.println(frase);
+
+    cont = cont + 1;
+
+    System.out.println("ACABOUUUUU")
+    }
+    
+}
+}
